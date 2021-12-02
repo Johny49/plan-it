fa148bcd3f5259cce8e9279e5a332acdd6860792
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-12-01 19:37:31 -0600

    added conditional class to style textareas based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,55 @@
-// display current day at top of page
-$("#currentDay").html(moment().format("dddd, MMMM DD, YYYY"));
+// add time blocks to planner with times from 8am to 5pm 
+function addElements() {
+    // display current day at top of page
+    var today = moment();
+    $("#currentDay").html(today.format("dddd, MMMM DD, YYYY"));
 
-    // add time blocks to planner with times from 8am to 5pm 
-function addTimeBlocks() {
     for (var i = 8; i <= 17; i++) {
         // format time for each section
         var time = moment(i, "HH").format("h A");
+        // append elements
         $(".container").append(`<section class='row time-block'>${time}</section>`);
-        $(".container").append(`<div class='col col-md-1 ${i}'></div>`);
-        $(".container").append("<textarea class='col col-md-10 description'></textarea>");        
+        $(".container").append(`<div class='col col-md-1 hour${i}'></div>`);
+
+        // style styling class based on time of day
+        console.log(i < today.format("HH"));
+        if (i > today.format("HH")) {
+            $(".container").append("<textarea class='col col-md-10 description future' ></textarea>");  
+        } else if (i == today.format("HH")) {
+            $(".container").append("<textarea class='col col-md-10 description present'></textarea>");  
+        } else {
+            $(".container").append("<textarea class='col col-md-10 description past'></textarea>");  
+        }
+      
         $(".container").append(`<button class='col col-md-1 btn saveBtn' id='${i}'></button>`);        
-        $(`#${i}`).append("<i class='row time-block;'></i>");
+        $(`#${i}`).append("<i class='fas fa-save'></i>");
     }
 }
 
 // retrieve saved items if they exist; otherwise create empty array
 function loadTasks() {
     var plannerTasks = JSON.parse(localStorage.getItem("planner-tasks")) || [];
-
     // add to planner
     for (task in plannerTasks) {
         // TODO: add to correct time
+        console.log(task);
     }
 }
 
 // save task to localStorage when save btn pressed
-function saveTask() {
+$(".container").on("click", "button", function() {
 // retrieve saved items if they exist; otherwise create empty array
-    var plannerTasks = JSON.parse(localStorage.getItem("planner-tasks")) || [];
+var plannerTasks = JSON.parse(localStorage.getItem("planner-tasks")) || [];
+var hourID = `hour${this.id}`
+// var task = $(`#${hourID}`).text()
 
-    var newTask = ["", ""]; //TODO fix this
-    plannerTasks.push(newTask);
-    // convert to string and store in localStorage
-    localStorage.setItem("planner-tasks", JSON.stringify(plannerTasks));
-};
+console.log($(`#${hourID}`).html());
+var newTask = [this.id, $(".hourID").text()]; //TODO fix this
+plannerTasks.push(newTask);
+// convert to string and store in localStorage
+localStorage.setItem("planner-tasks", JSON.stringify(plannerTasks));
+console.log(newTask);
+})
 
-addTimeBlocks();
-
-// $(this.click(function () {
-//     alert($(this).attr('setid'));
-// }))
-
-// // Event Handlers
-// $(".fa-save").click(saveTask);
+// add time blocks to page
+addElements();
